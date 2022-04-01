6a0f86676a77533fa021c460b1499eda2797df69
Malin Olsson <olssonmalin@live.com>
2022-04-01 17:59:13 +0200

    first commit

---
+++ src/Controller/Report.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class Report extends AbstractController
+{
+    /**
+     * @Route("/about", name="about")
+     */
+    public function about(): Response
+    {
+        $title = 'Om';
+
+        return $this->render('about.html.twig', [
+            'title' => $title
+        ]);
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function welcome(): Response
+    {
+        $title = 'Välkommen!';
+
+        return $this->render('home.html.twig', [
+            'title' => $title
+        ]);
+    }
+
+    /**
+     * @Route("/report", name="report")
+     */
+    public function report(): Response
+    {
+        $title = 'Redovisningstexter';
+        $path    = './../templates/texts/';
+        $files = scandir($path);
+        $files = array_diff(scandir($path), array('.', '..'));
+
+        return $this->render('report.html.twig', [
+            'title' => $title,
+            'files' => $files
+            ]);
+    }
+}
