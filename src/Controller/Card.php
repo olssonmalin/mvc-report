aaf6f7ea5b369ef4a46d51e79add16bc77d881b5
Malin Olsson <olssonmalin@live.com>
2022-04-23 11:42:10 +0200

    Lift out deal functions to their own module

--- src/Controller/Card.php
+++ src/Controller/Card.php
@@ -85,69 +85,69 @@ class Card extends AbstractController
         return $this->redirectToRoute('card');
     }
 
-    /**
-     * @Route("/card/deck/deal/{players}/{cards}", 
-     * name="deal", 
-     * methods={"GET","POST", "HEAD"})
-     */
-    public function deckDeal(int $players, int $cards, SessionInterface $session): Response
-    {
-        $title = 'Deal';
-        $session->set("deck", $session->get("deck") ?? new Deck());
-        $session->set("game", $session->get("game") ?? []);
-        $deck = $session->get("deck");
-        $game = $session->get("game");
-        $deck->shuffle();
-        try {
-            $game = [];
-            for ($i = 0; $i < $players; $i++)
-            {
-                $player = New Player($cards, $deck);
-                array_push($game, $player->getHand());
-            }
-        } catch (DeckTooSmallException $e) {
-            $this->addFlash("warning", "Not enough cards in the deck.");
-        }
+    // /**
+    //  * @Route("/card/deck/deal/{players}/{cards}", 
+    //  * name="deal", 
+    //  * methods={"GET","POST", "HEAD"})
+    //  */
+    // public function deckDeal(int $players, int $cards, SessionInterface $session): Response
+    // {
+    //     $title = 'Deal';
+    //     $session->set("deck", $session->get("deck") ?? new Deck());
+    //     $session->set("game", $session->get("game") ?? []);
+    //     $deck = $session->get("deck");
+    //     $game = $session->get("game");
+    //     $deck->shuffle();
+    //     try {
+    //         $game = [];
+    //         for ($i = 0; $i < $players; $i++)
+    //         {
+    //             $player = New Player($cards, $deck);
+    //             array_push($game, $player->getHand());
+    //         }
+    //     } catch (DeckTooSmallException $e) {
+    //         $this->addFlash("warning", "Not enough cards in the deck.");
+    //     }
 
-        return $this->render('card/card-deal.html.twig', [
-            'title' => $title,
-            'game' => $game,
-            'length' => $deck->getLen()
-        ]);
-    }
+    //     return $this->render('card/card-deal.html.twig', [
+    //         'title' => $title,
+    //         'game' => $game,
+    //         'length' => $deck->getLen()
+    //     ]);
+    // }
 
-     /**
-     * @Route(
-     *      "/card/deck/deal",
-     *      name="form-deal",
-     *      methods={"GET","HEAD"}
-     * )
-     */
-    public function dealForm(): Response
-    {   
-        $title = 'Deal';
-        return $this->render('form/deal.html.twig',[
-            'title' => $title
-        ]);
-    }
+    //  /**
+    //  * @Route(
+    //  *      "/card/deck/deal",
+    //  *      name="form-deal",
+    //  *      methods={"GET","HEAD"}
+    //  * )
+    //  */
+    // public function dealForm(): Response
+    // {   
+    //     $title = 'Deal';
+    //     return $this->render('form/deal.html.twig',[
+    //         'title' => $title
+    //     ]);
+    // }
 
-    /**
-     * @Route(
-     *      "/card/deck/deal",
-     *      name="form-deal-process",
-     *      methods={"POST"}
-     * )
-     */
-    public function dealProcess(Request $request): Response
-    {
-        $players = $request->request->get('players');
-        $cards  = $request->request->get('cards');
+    // /**
+    //  * @Route(
+    //  *      "/card/deck/deal",
+    //  *      name="form-deal-process",
+    //  *      methods={"POST"}
+    //  * )
+    //  */
+    // public function dealProcess(Request $request): Response
+    // {
+    //     $players = $request->request->get('players');
+    //     $cards  = $request->request->get('cards');
 
 
-        return $this->redirectToRoute('deal', [
-            'players' => $players,
-            'cards' => $cards
-        ]);
-    }
+    //     return $this->redirectToRoute('deal', [
+    //         'players' => $players,
+    //         'cards' => $cards
+    //     ]);
+    // }
 
 }
---
+++ src/Controller/Deal.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Card\Deck;
+use App\Card\Deck2;
+use App\Card\Player;
+use App\Card\DeckTooSmallException;
+
+class Deal extends AbstractController {
+    
+     /**
+     * @Route("/card/deck/deal/{players}/{cards}", 
+     * name="deal", 
+     * methods={"GET","POST", "HEAD"})
+     */
+    public function deckDeal(int $players, int $cards, SessionInterface $session): Response
+    {
+        $title = 'Deal';
+        $session->set("deck", $session->get("deck") ?? new Deck());
+        $session->set("game", $session->get("game") ?? []);
+        $deck = $session->get("deck");
+        $game = $session->get("game");
+        $deck->shuffle();
+        try {
+            $game = [];
+            for ($i = 0; $i < $players; $i++)
+            {
+                $player = New Player($cards, $deck);
+                array_push($game, $player->getHand());
+            }
+        } catch (DeckTooSmallException $e) {
+            $this->addFlash("warning", "Not enough cards in the deck.");
+        }
+
+        return $this->render('card/card-deal.html.twig', [
+            'title' => $title,
+            'game' => $game,
+            'length' => $deck->getLen()
+        ]);
+    }
+
+     /**
+     * @Route(
+     *      "/card/deck/deal",
+     *      name="form-deal",
+     *      methods={"GET","HEAD"}
+     * )
+     */
+    public function dealForm(): Response
+    {   
+        $title = 'Deal';
+        return $this->render('form/deal.html.twig',[
+            'title' => $title
+        ]);
+    }
+
+    /**
+     * @Route(
+     *      "/card/deck/deal",
+     *      name="form-deal-process",
+     *      methods={"POST"}
+     * )
+     */
+    public function dealProcess(Request $request): Response
+    {
+        $players = $request->request->get('players');
+        $cards  = $request->request->get('cards');
+
+
+        return $this->redirectToRoute('deal', [
+            'players' => $players,
+            'cards' => $cards
+        ]);
+    }
+
+}
