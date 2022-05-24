9f5ad3534cefd0b38b9c267c4c554eaf52ca0897
Malin Olsson <olssonmalin@live.com>
2022-05-24 15:55:05 +0200

    Add more card controller tests

--- tests/Controller/CardControllerTest.php
+++ tests/Controller/CardControllerTest.php
@@ -10,7 +10,35 @@ class CardControllerTest extends WebTestCase
     public function testRouteCard(): void
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/card');
+        $client->request('GET', '/card');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Card');
+    }
+
+    public function testRouteDeck(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/card/deck');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Deck');
+    }
+
+    public function testRouteDeckShuffle(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/card/deck/shuffle');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Shuffle');
+    }
+
+    public function testRouteResetDeck(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/card/deck/reset');
+        $client->followRedirect();
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h1', 'Card');
